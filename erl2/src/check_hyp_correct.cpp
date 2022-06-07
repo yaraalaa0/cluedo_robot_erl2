bbb5423fdd39accc455a9a35ab547683fe9db2b7
yaraalaa0 <37335420+yaraalaa0@users.noreply.github.com>
2022-06-07 22:32:52 +0000

    Add files via upload

---
+++ erl2/src/check_hyp_correct.cpp
@@ -0,0 +1,72 @@
+#include "erl2/check_hyp_correct.h"
+#include <unistd.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include <erl2/PlanningAction.h>
+#include "erl2/HypCompCheck.h"
+#include "erl2/Oracle.h"
+#include <string.h>
+#include <vector>
+
+ros::ServiceClient client_ont;
+ros::ServiceClient client_oracle;
+
+
+namespace KCL_rosplan {
+
+	CheckHypCorrectInterface::CheckHypCorrectInterface(ros::NodeHandle &nh) {
+	    // here the initialization
+	}
+
+	bool CheckHypCorrectInterface::concreteCallback(const rosplan_dispatch_msgs::ActionDispatch::ConstPtr& msg) {
+		// here the implementation of the action
+		
+		erl2::HypCompCheck h;    
+		client_ont.call(h);
+		ROS_INFO("I called the check complete hypothesis");
+		//std::string complete_hypotheses[] = h.response.complete_hyps;
+		std::vector<std::string> r = h.response.complete_hyps;
+		//std::cout<<"The complete hyps: "<<h.response.complete_hyps<<std::endl;
+		int length_hyp = r.size();
+		ROS_INFO("I checked the length of hypotheses");
+		if (length_hyp == 0){
+		    ROS_INFO("No complete hypothesis yet");
+		    return false;
+		}
+		else{
+		    ROS_INFO("There are some complete hypotheses");
+		    erl2::Oracle o;
+		    client_oracle.call(o);
+		    std::string correct_ID = std::to_string(o.response.ID);
+		    std::cout<<"Correct hypothesis ID: "<<correct_ID<<std::endl;
+		    std::cout<<"Current Complete Hypothesis IDs:"<<std::endl;
+		    bool correct_flag = false;
+		    for(int i=0; i<length_hyp; i++){
+		        std::cout<<r[i]<<std::endl;
+		        if (r[i] == correct_ID){
+		            correct_flag = true;
+		        }
+		    }
+		    if(correct_flag){
+		        ROS_INFO("Action (%s) performed: completed!", msg->name.c_str());
+		        return true;
+		    }
+		    else{
+		        return false;
+		    }
+		}
+		
+		        
+		
+	}
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "CheckHypCorrect_rosplan_action", ros::init_options::AnonymousName);
+	ros::NodeHandle nh("~");
+	client_ont = nh.serviceClient<erl2::HypCompCheck>("/check_hyp_complete");
+	client_oracle = nh.serviceClient<erl2::Oracle>("/oracle_solution");
+	KCL_rosplan::CheckHypCorrectInterface my_aci(nh);
+	my_aci.runActionInterface();
+	return 0;
+}
