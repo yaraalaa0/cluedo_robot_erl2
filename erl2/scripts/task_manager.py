00b9f3a4b3a17b9c9410da7b7291c94d6add3167
yaraalaa0 <37335420+yaraalaa0@users.noreply.github.com>
2022-06-30 22:31:17 +0200

    Update task_manager.py

--- erl2/scripts/task_manager.py
+++ erl2/scripts/task_manager.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
+"""
+.. module:: task_manager.py
+   :platform: Unix
+   :synopsis: this file is an implementation of the task manager node
+   
+.. moduleauthor:: Yara Abdelmottaleb
+ 
+This node implements the task manager. 
+It calls the problem generator, the planning server, the plan parser, and the plan dispatcher to start the execution of the PDDL task plan.
+It checks on the goal success result. If the goal wasn't successful, it updates the current knowledge and calls the problem generator, the planning server, the plan parser, and the plan dispatcher again.
+It keeps doing that until the goal is achieved successfully
+ 
+Subscribes to:
+   /rosplan_plan_dispatcher/action_dispatch
+ 
+Clients:
+   /rosplan_problem_interface/problem_generation_server
+   /rosplan_planner_interface/planning_server
+   /rosplan_parsing_interface/parse_plan
+   /rosplan_plan_dispatcher/dispatch_plan
+   /rosplan_knowledge_base/update
+   /rosplan_knowledge_base/update_array
+  
+"""
+
 import roslib
 import rospy
 from std_srvs.srv import Empty, EmptyRequest
@@ -14,17 +39,35 @@ last_dispatched_action = "" # a variable to store the last dispatched action nam
 hints_received = 0 # a variable to store the number of hints received (because there is a problem with the hints function in PDDL, the hints is reset to 0 every time the get_hint action fails)
 
 def clbk_ac_dispatch(msg):
+    """
+    This is the callback function for /rosplan_plan_dispatcher/action_dispatch subscriber
+    It receives the last dispatched action from the plan and stores it in the global variable last_dispatched_action to be checked later if the plan execution fails
+    If the dispatched action is get_hint, it increases the hints_received global variable
+    
+    Args:
+      msg(ActionDispatch): the dispatched action
+    
+    """
     global last_dispatched_action, hints_received
     last_dispatched_action = msg.name
     if msg.name == 'get_hint':
         hints_received = hints_received + 1
 
 def main():
+    """
+    This is the main function of the node
+    It initializes the node handle, the action dispatch subscriber, and the service clients /rosplan_problem_interface/problem_generation_server , /rosplan_planner_interface/planning_server,
+    /rosplan_parsing_interface/parse_plan, /rosplan_plan_dispatcher/dispatch_plan , /rosplan_knowledge_base/update , and /rosplan_knowledge_base/update_array
+    
+    It calls the problem generator, the planning server, the plan parser, and the plan dispatcher to start the execution of the PDDL task plan.
+    It checks on the goal success result. If the goal wasn't successful, it updates the current knowledge and calls the problem generator, the planning server, the plan parser, and the plan dispatcher again.
+    It keeps doing that until the goal is achieved successfully
+    
+    """
     global last_dispatched_action, hints_received
     
     # initialize the ROS node
     rospy.init_node('task_manager')
-                    
     
     # Initialize the rosplan services clients
     problem_client = rospy.ServiceProxy('/rosplan_problem_interface/problem_generation_server',Empty)
